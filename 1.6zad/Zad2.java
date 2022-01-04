2981e92e68bfa6d47a3311b96e5a074b543b2fb3
cyanLambda <sglfdghlcmu@gmail.com>
2022-01-05 00:17:27 +0200

    zad[1,2]

---
+++ 1.6zad/Zad2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Zad1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String phase = sc.nextLine();
+        System.out.println("ilość znaków w ["+phase+"]: "+phase.length());
+    }
+}
--- 1.6zad/zad1.java
+++ 1.6zad/zad1.java
@@ -1,6 +1,15 @@
-class Zad1{
+import java.util.*;
+public class Zad1{
     public static void main(String[] args){
-        
+        Scanner sc = new Scanner(System.in);
+        String H = "haslo";
 
+        String password = sc.nextLine();
+        if(password.equals(H)){
+            System.out.println("haslo ["+H+"] jest poprawne");
+        }
+        else{
+            System.out.println("NIEPOPRAWNE HASLO");
+        }
     }
 }
